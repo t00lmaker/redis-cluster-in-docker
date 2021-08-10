a8a359d213a511c4f78dc4c5b6674dc846b4dfdf
Luan Pontes <luanpontes2@gmail.com>
2021-08-10 19:59:09 -0300

    Update redis-cluster-config.sh
    add server option configure ip

--- redis-cluster-config.sh
+++ redis-cluster-config.sh
@@ -3,8 +3,12 @@
 rm -rf 700* && rm -rf *.aof && rm -rf *.conf && rm -rf *.rdb ;
 
 sleep 5;
+ 
+if [ -z "$IP" ]; then
+    IP=$(hostname -I)
+fi
 
-IP=$(hostname -I)
+echo "IP: '$IP'"
 
 for port in `seq 7001 7006`; do \
   echo "> ${port}"
@@ -23,6 +27,6 @@ for port in `seq 7001 7006`; do \
   sleep 3;
 done
 
-echo "yes" | redis-cli -p 7001 --cluster create 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005 127.0.0.1:7006 --cluster-replicas 1;
+echo "yes" | redis-cli -p 7001 --cluster create $IP:7001 $IP:7002 $IP:7003 $IP:7004 $IP:7005 $IP:7006 --cluster-replicas 1;
 
 tail -f ./7001/logs
